03f536c59c42fc4f3eb3743a0b61d8e06fcdc207
udisch <udisch@9836e98b-dcfd-4841-d3f1-705d572d7d26>
2010-07-02 20:28:18 +0000

    presentation version

--- background.m
+++ background.m
@@ -1,5 +1,5 @@
 function [ back ] = background( filename )
-%Reads avi file and performs background subtraction
+%Reads avi file and gets background
 
 mov = read_avi(filename);
 back= median(mov,3);
